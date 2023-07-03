378dffd2196140f426b18f4567d5879b4366c450
SantoshiSamal <santoshisamal.14@gmail.com>
2023-07-03 13:08:58 -0400

    Modified Budget allocation Project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
+import Currency from './components/Currency'
 // Add code to import the other components here under
 import RemainingBudget from './components/Remaining'
 import ExpenseTotal from './components/ExpenseTotal'
@@ -26,6 +26,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, dispatch, currency, expenses } = useContext(AppContext);
+  const inputRef = useRef(null);
+
+  const handleScroll = (event) => {
+    const deltaY = event.deltaY;
+    if (deltaY < 0 && budget < 20000) {
+      dispatch({ type: 'SET_BUDGET', payload: budget + 10 });
+    } else if (deltaY > 0 && budget > getTotalExpenses()) {
+      dispatch({ type: 'SET_BUDGET', payload: budget - 10 });
+    }
+  };
+
+  const handleOnChange = (e) => {
+    const newBudget = parseInt(e.target.value);
+    if (!isNaN(newBudget) && newBudget >= getTotalExpenses() && newBudget <= 20000) {
+      dispatch({ type: 'SET_BUDGET', payload: newBudget });
+    } else if (newBudget < getTotalExpenses()) {
+      alert('Budget cannot be lower than total expenses');
+    } else {
+      alert('Budget value should be a number and should not exceed 20,000');
+    }
+  };
+
+  const getTotalExpenses = () => {
+    return expenses.reduce((total, item) => {
+      return (total += item.cost);
+    }, 0);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = budget;
+    }
+  }, [budget]);
+
+  return (
+    <div className='alert alert-secondary' onWheel={handleScroll}>
+      <span>Budget: {currency}</span>
+      <input
+        type='number'
+        id='budget'
+        value={budget}
+        ref={inputRef}
+        onChange={handleOnChange}
+        min={getTotalExpenses()}
+        max={20000}
+     
+      />
+    </div>
+  );
 };
+
 export default Budget;
